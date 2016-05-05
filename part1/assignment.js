ccb65d33ca59369a06389416adcb6cb3ece24b59
Ryan Sobol <contact@ryansobol.com>
2016-05-05 16:18:35 -0700

    Fix leap year solution

--- part1/assignment.js
+++ part1/assignment.js
@@ -327,7 +327,9 @@ function iceCreamPosition(pieTemperature, iceCreamFlavor) {
 function isLeapYear(year) {
   if (year % 4 !== 0) {
     return false;
-  } else if (year % 100 === 0 && year % 400 === 0) {
+  } else if (year % 100 !== 0) {
+    return true;
+  } else if (year % 400 !== 0) {
     return false;
   } else {
     return true;
