1ffe03dd5bafe81e5ea2a56865eccfad77e813d2
Ken McGrady <ken.mcgrady@gmail.com>
2016-05-05 23:45:13 -0700

    Add the bonus

--- part2/bonus.js
+++ part2/bonus.js
@@ -1,6 +1,7 @@
 
-// Define a function named group that is given an array of objects and a string,
-// key.
+// Define a function named group that takes in two arguments:
+//     arr (an array of objects)
+//     key (string)
 //
 // Returns an object containing collections grouped by the value of each object
 // at that key
@@ -21,11 +22,61 @@
 //     diamonds: [{name: 'ace', suit: 'diamonds'}, ...]
 //     clubs: [{name: 'ace', suit: 'clubs'}, ...]
 //   }
+function group(arr, key) {
+  var result = {}
+  for(var i = 0; i < arr.length; i++) {
+    if (result[arr[i][key]] === undefined) {
+      result[arr[i][key]] = [];
+    }
+    result[arr[i][key]].push(arr[i]);
+  }
 
+  return result;
+}
 
-// Define a function named merge that takes in two arrays of numbers.
-// We can assume that the numbers in each array are sorted.
+// Define a function named merge that takes in two arguments:
+//    arr1 (sorted array of numbers)
+//    arr2 (sortedarray of numbers)
 //
 // Return a new array that contains all the numbers in each array still sorted.
+// Example: given [1, 3, 5] and [2, 4], it would produce [1, 2, 3, 4, 5]
+// Another example: given [1, 3, 5], [2, 2], it would produce [1, 2, 2, 3, 5]
+//
 // Tip: Each array may contain duplicate numbers. You need to ensure that each
 // number is still accounted for.
+// Another Tip: Each array is sorted, which makes the solution easier.
+function merge(arr1, arr2) {
+  var output = [];
+  while(arr1.length > 0 && arr2.length > 0) {
+    if (arr1[0] < arr2[0]) {
+      output.push(arr1.shift());
+    } else {
+      output.push(arr2.shift());
+    }
+  }
+
+  while(arr1.length > 0) {
+    output.push(arr1.shift());
+  }
+
+  while(arr2.length > 0) {
+    output.push(arr2.shift());
+  }
+
+  return output;
+}
+
+// Write a function named bigSum that takes in an arbitrary number of
+// parameters. We can assume all parameters are numbers.
+//
+// Returns the sum of all of the parameters. For example
+// bigSum(1, 2, 3, 4) produces 10.
+// If there are no parameters, return 0.
+// Tip: Think about how this is different from sum.
+function bigSum() {
+  var sum = 0;
+  for(var i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
+}
--- part2/bonus.test.js
+++ part2/bonus.test.js
@@ -1,23 +1,59 @@
-suite('#group', function() {
-  test('groups arrays of objects by a specific key', function() {
-    assert.equal(group([], 'foo'), {});
-    assert.equal(group([{}], 'foo'), {});
+window.onload = function() {
+  var assert = chai.assert;
+  mocha.setup('tdd');
+  suite('#group', function() {
+    test('groups empty arrays correctly', function() {
+      assert.deepEqual(group([], 'foo'), {});
+    });
+
+    var SUITS = ['spades', 'hearts', 'clubs', 'diamonds'];
+    var VALUES = ['A', 'K'];
+    var cards = [];
+    for (var i = 0; i < SUITS.length; i++) {
+      for (var j = 0; j < VALUES.length; j++) {
+        cards.push({
+          value: VALUES[j],
+          suit: SUITS[i]
+        });
+      }
+    }
+    var grouped = group(cards, 'suit');
+    test('produces the proper groups', function() {
+      assert.sameMembers(Object.keys(grouped), SUITS);
+    });
+
+    test('produces the proper members for each group', function() {
+      assert.sameDeepMembers(grouped.hearts, [
+        {value: 'A', suit: 'hearts'}, {value: 'K', suit: 'hearts'}
+      ]);
+      assert.sameDeepMembers(grouped.clubs, [
+        {value: 'A', suit: 'clubs'}, {value: 'K', suit: 'clubs'}
+      ]);
+      assert.sameDeepMembers(grouped.spades, [
+        {value: 'A', suit: 'spades'}, {value: 'K', suit: 'spades'}
+      ]);
+      assert.sameDeepMembers(grouped.diamonds, [
+        {value: 'A', suit: 'diamonds'}, {value: 'K', suit: 'diamonds'}
+      ]);
+    });
   });
 
-  test('returns false if the letter is not a vowel', function() {
-    assert.equal(isVowel('z'), false);
-    assert.equal(isVowel('b'), false);
-    assert.equal(isVowel('d'), false);
-    assert.equal(isVowel('y'), false);
+  suite('#merge', function() {
+    test('merges two sorted arrays', function() {
+      assert.deepEqual(merge([1, 3, 5], [2, 4, 6]), [1, 2, 3, 4, 5, 6]);
+      assert.deepEqual(merge([1, 3, 5], [1, 2]), [1, 1, 2, 3, 5]);
+      assert.deepEqual(merge([], [1, 2]), [1, 2]);
+      assert.deepEqual(merge([1, 2], []), [1, 2]);
+    });
   });
 
-});
-
-suite('#merge', function() {
-  test('merges two sorted arrays', function() {
-    assert.equal(merge([1, 3, 5], [2, 4, 6]), [1, 2, 3, 4, 5, 6]);
-    assert.equal(merge([1, 3, 5], [1, 2]), [1, 1, 2, 3, 4, 5]);
-    assert.equal(merge([], [1, 2]), [1, 2]);
-    assert.equal(merge([1, 2], []), [1, 2]);
+  suite('#bigSum', function() {
+    test('adds a bunch of numbers as parameters', function() {
+      assert.strictEqual(bigSum(), 0);
+      assert.strictEqual(bigSum(1, 2, 3, 4), 10);
+      assert.strictEqual(bigSum(1, -1, 2, -3), -1);
+    });
   });
-});
+
+  mocha.run();
+}
